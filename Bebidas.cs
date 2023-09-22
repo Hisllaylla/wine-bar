fd2131b9c4a38c4b4c836068429bd5e27629f2e3
Hisllaylla <hisllayllak@gmail.com>
2023-09-21 22:03:29 -0300

    update

--- Bebidas.cs
+++ Bebidas.cs
@@ -1,16 +1,7 @@
 namespace WineBar;
 
-public class Bebidas
+public record Bebidas(string Nome, int Conteudo, double TeorAlcoolico, string TipoUva, double Preco, string Descricao, string Pais, List<Bebidas> CatalogoVinhos)
 {
-    public string Nome { get; set; }
-    public int Conteudo { get; set; }
-    public double TeorAlcoolico { get; set; }
-    public string TipoUva { get; set; }
-    public double Preco { get; set; }
-    public string Descricao { get; set; }
-    public string Pais { get; set; }
-    public List<Bebidas> CatalogoVinhos { get; set; }
-
     public override string ToString()
     {
         return $"{Nome} {Conteudo}ml, com teor alcoólico de {TeorAlcoolico}%, provindo de {Pais} por {Preco}BRL. Produzida principalmente pela(s) uva(s): {TipoUva}. {Descricao}.";
